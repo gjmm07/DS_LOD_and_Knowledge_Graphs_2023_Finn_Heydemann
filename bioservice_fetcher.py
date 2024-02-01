cf15126ae69f83f7e1c91ad0f6869006c3a6add4
gjmm07 <finn.h@hotmail.de>
2024-02-01 13:13:57 +0100

    queried all possible data from NCBI - search for impportant information

--- bioservice_fetcher.py
+++ bioservice_fetcher.py
@@ -1,8 +1,6 @@
 import time
 from bioservices import EUtils
 import pandas as pd
-import urllib
-import numpy as np
 from operator import itemgetter
 
 _s = EUtils(email="finn.heydemann@th-koeln.de")
@@ -13,17 +11,26 @@ def _fetch_refseq_nucleotid(refseq_nucleotid_id: str) -> dict[str, str]:
     Based on a ncbi refseq nucleotid ID the encoded protein, gene and bacterial strain is returned 
     """
     found_data = {"gene": "", "protein": "", "genome": "", "organism": ""}
+    print(refseq_nucleotid_id)
+    time.sleep(0.5)  # wait to ensure to not be blocked
     try:
         raw_data = _s.EFetch("nucleotide", refseq_nucleotid_id, retmode="dict", rettype="summary")["GBSet"]["GBSeq"]
-    except AttributeError:
+    except (AttributeError, TypeError):
         return found_data
-    except TypeError:
-        print(refseq_nucleotid_id)
-    time.sleep(0.5)  # wait to ensure to not be blocked
     definition = raw_data["GBSeq_definition"]
     if "gene" in definition:
-        gene, protein = definition.split("gene for")
-        protein = protein.replace(", complete CDS", "")
+        try:
+            gene, protein = definition.split("gene for")
+            # protein = protein.replace(", complete CDS", "")
+            # organism = raw_data["GBSeq_organism"]
+            # found_data["gene"] = gene.strip()
+            # found_data["protein"] = protein.strip()
+            # found_data["organism"] = organism.strip()
+        except ValueError:
+            gene, protein = definition.split("gene")
+        idx = protein.upper().find(", COMPLETE CDS")
+        protein = protein[:idx]
+        # protein = protein.upper().replace(", COMPLETE CDS", "")
         organism = raw_data["GBSeq_organism"]
         found_data["gene"] = gene.strip()
         found_data["protein"] = protein.strip()
@@ -40,12 +47,16 @@ def _fetch_refseq_protein(refseq_protein_id: str) -> tuple[str, str, str]:
     Based on the ncbi refseq protein ID the parent taxon, the protein name is returned
     If needed the same parent taxon is returned based on another entry
     """
+    print(refseq_protein_id)
+    time.sleep(0.5)  # wait to ensure to not be blocked
     try:
         data = _s.EFetch("protein", refseq_protein_id, retmode="dict", rettype="summary")["GBSet"]["GBSeq"]
-    except AttributeError:
+    except (AttributeError, TypeError):
         return "", "", ""
-    time.sleep(0.5)  # wait to ensure to not be blocked
-    parent_taxon: str = data["GBSeq_organism"].strip()
+    parent_taxon: str or list[str] = data["GBSeq_organism"]
+    if isinstance(parent_taxon, list):
+        parent_taxon = parent_taxon[-1]
+    parent_taxon = parent_taxon.strip()
     definition = data["GBSeq_definition"]
     start_index, stop_index = (n := definition.find("[")), definition.find("]", n)
     same_parent_taxon: str = definition[start_index + 1: stop_index].strip()
@@ -55,13 +66,14 @@ def _fetch_refseq_protein(refseq_protein_id: str) -> tuple[str, str, str]:
 
 
 def _fetch_genbank_protein(genbank_protein: str) -> tuple[str, str]:
+    print(genbank_protein)
     strain, organism = "", ""
+    time.sleep(0.5)
     try:
         data = _s.EFetch("protein", genbank_protein, retmode="dict", rettype="summary")["GBSet"]["GBSeq"]
-    except AttributeError:
+    except (AttributeError, TypeError):
         return organism, strain
     # GB source
-    time.sleep(0.5)
     for t in data["GBSeq_feature-table"]["GBFeature"]:
         if t["GBFeature_key"] == "source":
             for x in t["GBFeature_quals"]["GBQualifier"]:
@@ -74,9 +86,15 @@ def _fetch_genbank_protein(genbank_protein: str) -> tuple[str, str]:
 
 def _fetch_genbank_nucleotide(genbank_nucleotide: str) -> tuple[str, str]:
     time.sleep(0.5)
-    data = _s.EFetch("nucleotide", genbank_nucleotide, retmode="dict", rettype="summary")
-    data = data.get("GBSet").get("GBSeq").get("GBSeq_feature-table").get("GBFeature")
+    print(genbank_nucleotide)
     strain, organism = "", ""
+    try:
+        data = _s.EFetch("nucleotide", genbank_nucleotide, retmode="dict", rettype="summary")
+    except (AttributeError, TypeError):
+        return organism, strain
+    data = data.get("GBSet").get("GBSeq").get("GBSeq_feature-table").get("GBFeature")
+    if not isinstance(data, list):
+        data = [data]
     for t in data:
         if t["GBFeature_key"] == "source":
             for x in t["GBFeature_quals"]["GBQualifier"]:
@@ -84,6 +102,13 @@ def _fetch_genbank_nucleotide(genbank_nucleotide: str) -> tuple[str, str]:
                     organism = x["GBQualifier_value"]
                 if x["GBQualifier_name"] == "strain":
                     strain = x["GBQualifier_value"]
+    #elif isinstance(data, dict):
+    #    if data["GBFeature_key"] == "source":
+    #        for x in data["GBFeature_quals"]["GBQualifier"]:
+    #            if x["GBQualifier_name"] == "organism":
+    #                organism = x["GBQualifier_value"]
+    #            if x["GBQualifier_name"] == "strain":
+    #                strain = x["GBQualifier_value"]
     return organism, strain
 
 
@@ -100,8 +125,8 @@ def get_protein_and_parent(df_row: pd.Series) -> pd.Series:
     """
     To a pandas dataframe row this function return a pd Series with the bacteria parent taxon and the protein it encodes
     """
-    parent_taxon, protein, *_ = _fetch_refseq_protein(df_row.refseq_protein_accession)
-    return pd.Series([parent_taxon, protein])
+    parent_taxon, protein, same_parent_taxon = _fetch_refseq_protein(df_row.refseq_protein_accession)
+    return pd.Series([parent_taxon, protein, same_parent_taxon])
 
 
 def get_strain_and_gene(df_row: pd.Series) -> pd.Series:
@@ -127,28 +152,25 @@ def get_organism_strain_via_nuc(df_row: pd.Series) -> pd.Series:
 
 
 if __name__ == "__main__":
-    url = "https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/ReferenceGeneCatalog.txt"
-    df = pd.read_csv(urllib.request.urlopen(url), delimiter="\t")
+    # url = "https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/ReferenceGeneCatalog.txt"
+    # df = pd.read_csv(urllib.request.urlopen(url), delimiter="\t")
     #
-    # sample_df = df.sample(5, random_state=42)
-
-    print(_fetch_refseq_nucleotid("NG_052278.1"))
-    print(df)
+    # print(_fetch_refseq_nucleotid("NG_049864.1"))
+    # NG_049864.1
+    # WP_001206316.1
+    # AJTQ01000183.1
 
     # df[["refseq_gene",
-    #     "refseq_protein",
-    #     "refseq_genome",
-    #     "refseq_organism"]] = df.apply(get_strain_and_gene, axis=1)
+    #    "refseq_protein",
+    #    "refseq_genome",
+    #    "refseq_organism"]] = df.apply(get_strain_and_gene, axis=1)
 
-    print(0)
-    # df[["refseq_parent", "rrefseq_protein"]] = df.apply(get_protein_and_parent, axis=1)
+    df = pd.read_csv("samples.csv")
 
-    print(1)
+    # df[["refseq_parent", "rrefseq_protein", "refseq_same_parent"]] = df.apply(get_protein_and_parent, axis=1)
 
     # df[["genbank_organsim_nuc", "genbank_strain_nuc"]] = df.apply(get_organism_strain_via_nuc, axis=1)
 
-    print(2)
-
-    # df[["genbank_organsim_prot", "genbank_strain_prot"]] = df.apply(get_organism_strain_via_prot, axis=1)
+    df[["genbank_organsim_prot", "genbank_strain_prot"]] = df.apply(get_organism_strain_via_prot, axis=1)
 
     df.to_csv("samples.csv")
